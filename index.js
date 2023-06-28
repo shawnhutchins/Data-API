a3a4b0d56ecc2bfbab7e5fb586a0135653a70060
shawn <shawnhutchins1@gmail.com>
2023-06-27 21:00:13 -0400

    Connected database

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 require('dotenv').config()
+
 const express = require('express')
 const mongoose = require('mongoose')
+const mongoString = process.env.DATABASE_URL
 const routes = require('./routes/routes.js')
+
+mongoose.connect(mongoString)
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+database.once('connected', () => {
+    console.log('Database Connected')
+})
 
 const app = express()
 
 app.use('/api', routes)
 app.use(express.json())
+
 app.listen(3000, () => {
     console.log(`Server started at ${3000}`)
 })
